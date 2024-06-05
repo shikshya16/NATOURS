705dcb5597ec17f7e120c51a903c6ffdad04b95d
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-05 08:29:12 +0545

    Handle uncaught exceptions and unhandled rejections to gracefully shutdown the server

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException' , err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name , err.message);
+        process.exit(1);
+});
+
+
 dotenv.config({path: './config.env'});
 
 mongoose.connect( process.env.DATABASE)
@@ -9,13 +16,18 @@ mongoose.connect( process.env.DATABASE)
 
 
 const port = process.env.port || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App is running on port ${port}...`);
 });
 
 
-
-
+process.on('unhandledRejection' , err => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name , err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
 
 
 
