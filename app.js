2e9dc88c0dd1b65d9ed7fb435e4f6a9188144b12
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-05 08:15:28 +0545

    Update route handlers to handle unknown routes with a 404 error

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes')
 
@@ -10,14 +13,8 @@ if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next)=> {
-    console.log('Hello From the middleware');
-    next();
-});
 
 app.use((req, res , next) => {
     req.requestTime = new Date().toISOString();
@@ -27,6 +24,25 @@ app.use((req, res , next) => {
 
 app.use('/api/v1/tours' , tourRouter);
 app.use('/api/v1/users' , userRouter);
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status : 'fail' ,
+    //     message : `Can't find ${req.originalUrl} on this server!`
+    // });
+
+    // const err = new Error( `Can't find ${req.originalUrl} on this server!`);
+    // err.status = 'fail';
+    // err.statusCode = 404 ;
+
+    //next(err);
+
+    next(new AppError( `Can't find ${req.originalUrl} on this server!`));
+});
+
+
+
+app.use(globalErrorHandler);
 
 module.exports = app ;
 
