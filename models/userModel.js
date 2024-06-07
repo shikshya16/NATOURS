efb076725887dc9232a4835ebaa709f81107003b
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-07 10:19:32 +0545

    Implement methods to compare passwords and check if password has been changed after JWT issue.

--- models/userModel.js
+++ models/userModel.js
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema({
     password : {
         type : String ,
         required : [ true , 'Please provide a password'] ,
-        minlength : 8 
+        minlength : 8 ,
+        select : false
     } ,
 
     confirmPassword : {
@@ -34,7 +35,8 @@ const userSchema = new mongoose.Schema({
             },
             message : 'Passwords are not same!'
         }
-    } 
+    },
+    passwordChangedAt : Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -48,6 +50,26 @@ userSchema.pre('save', async function(next) {
     this.confirmPassword = undefined;
     next();
 });
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword, 
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword , userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if(!this.passwordChangedAt){
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000 , 
+            10
+);
+
+        return JWTTimestamp < changedTimestamp;
+    }
+    // false means not changed
+   return false ; 
+}
 
 const User = mongoose.model('User', userSchema);
 
