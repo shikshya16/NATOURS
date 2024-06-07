731f343981df27c11392b054c85cdfd41352c296
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-07 10:14:19 +0545

    Load environment variables from config.env.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 process.on('uncaughtException' , err => {
-    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(`UNCAUGHT EXCEPTION! Shutting down...`);
     console.log(err.name , err.message);
         process.exit(1);
 });
@@ -22,7 +22,7 @@ const server = app.listen(port, () => {
 
 
 process.on('unhandledRejection' , err => {
-    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(`UNHANDLED REJECTION! Shutting down...`);
     console.log(err.name , err.message);
     server.close(() => {
         process.exit(1);
