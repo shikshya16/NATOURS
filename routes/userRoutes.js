51be90bc7af3c5d5a05cc36245b8be866196cdec
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-07 18:54:38 +0545

    feat: Implement user routes and controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,27 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch(
+    '/updateMyPassword' , 
+    authController.protect , 
+    authController.updatePassword
+);
+
+router.delete(
+    '/deleteMe' , 
+    authController.protect , 
+    userController.deleteMe
+);
+
+router.patch(
+    '/updateMe' , 
+    authController.protect , 
+    userController.updateMe
+);
+
 router
     .route('/')
     .get(userController.getAllUsers)
