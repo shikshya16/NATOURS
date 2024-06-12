b9cd3e096d988a08ba31e4138ee4bab6b2f9a512
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-12 15:09:09 +0545

    Add aggregation middleware to exclude secret tours from aggregate queries.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
 
@@ -39,7 +40,8 @@ const tourSchema = new mongoose.Schema({
         type : Number,
         default : 5.00,
         min : [1 , 'Rating must be above 1.0'],
-        max : [10, 'Rating must be below 10.0']
+        max : [10, 'Rating must be below 10.0'],
+        set : val => Math.round(val * 10) / 10
     }, 
 
     ratingsQuantity : {
@@ -91,7 +93,39 @@ const tourSchema = new mongoose.Schema({
     secretTour : {
         type : Boolean,
         default : false
-    }
+    },
+
+    startLocation : {
+        //GeoJSON
+        type: {
+            type : String,
+            default : 'Point',
+            enum: ['Point']
+        },
+        coordinates : [Number],
+        address : String ,
+        description : String
+    },
+
+    locations:[
+        {
+            type : {
+                type : String ,
+                default : 'Point',
+                enum : ['Point']
+            } ,
+            coordinates : [Number],
+            address : String ,
+            description : String,
+            day : Number
+        }
+    ],
+    guides : [
+        {
+            type : mongoose.Schema.ObjectId ,
+            ref : 'User'
+        }
+    ]
  } , 
  {
     toJSON : { virtuals : true },
@@ -99,15 +133,35 @@ const tourSchema = new mongoose.Schema({
  }
 );
 
+// tourSchema.index({price : 1});
+tourSchema.index({price : 1, ratingsAverage : -1});
+tourSchema.index({ slug : 1 });
+tourSchema.index({ startLocation: '2dsphere'});
+
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7 ;
 });
+
+//Virtual Populate
+tourSchema.virtual('reviews', {
+    ref : 'Review',
+    foreignField : 'tour',
+    localField : '_id'
+})
 
 //DOCUMENT MIDDLEWARE : RUNS BEFORE .save() and .create() 
 tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, { lower : true });
     next();
 });
+
+// tourSchema.pre('save' , async function(next) {
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
+
+
 
 // tourSchema.pre('save' , function(next) {
 //     console.log('Will Save document...');
@@ -127,17 +181,26 @@ tourSchema.pre(/^find/, function(next) {
     next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+    this.populate({
+      path: 'guides',
+      select: '-__v -passwordChangedAt'
+    });
+    next();
+  });
+
 tourSchema.post(/^find/, function(docs, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds!`);
     next();
 });
 
+
 //AGGREGATION  MIDDLEWARE
-tourSchema.pre('aggregate' , function(next){
-    this.pipeline().unshift({ $match : { secretTour : { $ne : true} } });
-    console.log(this.pipeline());
-    next();
-})
+// tourSchema.pre('aggregate' , function(next){
+//     this.pipeline().unshift({ $match : { secretTour : { $ne : true} } });
+//     console.log(this.pipeline());
+//     next();
+// })
 
 const Tour = mongoose.model('Tour', tourSchema);
 
