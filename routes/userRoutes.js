25f6bb2487ac0064a01d28e0fbf56d7e1beb6b0d
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-12 15:11:56 +0545

    Integrate admin functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
+
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -11,23 +12,31 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+//protect all routes after this middleware
+router.use(authController.protect);
+
 router.patch(
-    '/updateMyPassword' , 
-    authController.protect , 
+    '/updateMyPassword' ,  
     authController.updatePassword
 );
 
+router.get(
+        '/me',  
+        userController.getMe , 
+        userController.getUser
+    );
+
 router.delete(
     '/deleteMe' , 
-    authController.protect , 
     userController.deleteMe
 );
 
 router.patch(
-    '/updateMe' , 
-    authController.protect , 
+    '/updateMe' ,  
     userController.updateMe
 );
+
+router.use(authController.restrictTo('admin'));
 
 router
     .route('/')
@@ -39,5 +48,6 @@ router
     .get(userController.getUser)
     .patch(userController.updateUser)
     .delete(userController.deleteUser);
+
 
 module.exports = router ;
